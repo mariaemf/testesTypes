66288ecd83d67e4f2425c0c455f01b6a5c213dee
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-02 19:19:05 -0300

    criacao de dois componentes

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Projeto TS</h1>
+      </header>
+      <h1>Conteudo...</h1>
+      <footer>
+        <p>
+          <span>Projeto TS</span> @ 2022
+        </p>
+      </footer>
+    </div>
+  );
+}
+export default App;
