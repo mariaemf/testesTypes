8179091e377b0b86b6739b6764046c92f4a914ca
mariaemf <mariaeduardamartinsf@gmail.com>
2022-12-03 17:10:09 -0300

    adicionado alguns elementos de estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 
 function App() {
-  return <Header />;
-  <Footer />;
+  return <Header className={StyleSheet.header} />;
+  <footer />;
 }
 export default App;
